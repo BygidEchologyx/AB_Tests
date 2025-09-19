af6ccd2c34219b242936599f1ff5cbc27818ff1a
BygidEchologyx <bygid@echologyx.com>
2025-09-19 15:03:52 +0600

    updated ID 85

--- websites/Chocolade Bezorgd/ID 85/variation 01/index.js
+++ websites/Chocolade Bezorgd/ID 85/variation 01/index.js
@@ -1,8 +1,8 @@
 (function () {
   const config = {
-    test_id: "ID1_variation",
-    test_name: "ID1_Home_afspraak_planner",
-    selector: "#werkplaatstplanner-banner",
+    test_id: "ID85_variation",
+    test_name: "ID85_Waarde_propositie_V2",
+    selector: ".woocommerce-product-details__short-description",
   };
 
   console.log("=== Experiment bucketed ===", config.test_name, config.test_id);
@@ -26,7 +26,9 @@
   }
 
   waitForElement(
-    () => !!document.querySelector(config.selector),
+    () =>
+      !!document.querySelector(config.selector) &&
+      !!document.querySelector(".cb-product-usps"),
     () => {
       let echoVariation = {
         init: function () {
@@ -52,26 +54,107 @@
           styles.setAttribute("type", "text/css");
           styles.setAttribute("id", config.test_id + "-style");
           document.head.appendChild(styles).textContent = `
-            html {
-              position: relative;
-
-              &::before {
-                content: "AB test is running";
-                position: fixed;
-                top: 0;
-                left: 0;
-                z-index: 99999999999;
-                background: #ff0000;
-                color: #ffffff;
-                padding: 10px;
-                border: 7px solid #269b11;
+              .woocommerce-product-details__short-description {
+                margin-top: 12px;
+                margin-left: 5px;
               }
-            }
+
+              .id85-product-bullet {
+                display: flex;
+                align-items: center;
+                gap: 8px;
+              }
+
+              .id85-product-bullet-icon {
+                width: 5px;
+                height: 5px;
+                background: black;
+                border-radius: 50%;
+              }
+
+              .id85-bullet-text-content,
+              .id85-bullet-text-content > strong,
+              .id85-usp-text-content,
+              .id85-usp-text-content > strong {
+                font-family: "Open Sans";
+                font-size: 15px;
+                color: #242223 !important;
+              }
+
+              .id85-usp-text-content > strong {
+                color: #457B53 !important;
+              }
            `;
         },
 
         mainJS: function () {
           console.log("=== Main JS running ===", config.test_name);
+
+          const bulletNumber = document
+            .querySelector(".button-alignment-wrapper.align-left a.active")
+            .textContent.slice(0, 2);
+
+          const firstBulletText = document
+            .querySelector(".product_title.entry-title")
+            .textContent.includes("zelf samenstellen")
+            ? `<strong>Selecteer</strong> zelf je ${bulletNumber} favoriete bonbons`
+            : `<strong>${bulletNumber} bestseller</strong> bonbons (alcoholvrij)`;
+
+          const updatedUspTexts = `
+            <div class="cb-product-usp">
+              <div class="cb-product-usp-icon"></div>
+              <div class="cb-product-usp-text">
+                <span class="id85-usp-text-content"><strong>Gratis</strong> verzending vanaf € 35</span>
+              </div>
+            </div>
+            <div class="cb-product-usp">
+              <div class="cb-product-usp-icon"></div>
+              <div class="cb-product-usp-text">
+                <span class="id85-usp-text-content">Kies zelf de gewenste <strong>verzenddatum</strong></span>
+              </div>
+            </div>
+            <div class="cb-product-usp">
+              <div class="cb-product-usp-icon"></div>
+              <div class="cb-product-usp-text">
+                <span class="id85-usp-text-content">Voor 17:00 uur besteld, <strong>vandaag</strong> verzonden</span>
+              </div>
+            </div>
+          `;
+
+          const updatedBulletPoints = `
+            <div class="id85-product-bullet">
+              <div class="id85-product-bullet-icon"></div>
+              <div class="id85-product-bullet-text">
+                <span class="id85-bullet-text-content">${firstBulletText}</span>
+              </div>
+            </div>
+            <div class="id85-product-bullet">
+              <div class="id85-product-bullet-icon"></div>
+              <div class="id85-product-bullet-text">
+                <span class="id85-bullet-text-content"><strong>Vers & handgemaakt</strong> door onze chocolatiers</span>
+              </div>
+            </div>
+            <div class="id85-product-bullet">
+              <div class="id85-product-bullet-icon"></div>
+              <div class="id85-product-bullet-text">
+                <span class="id85-bullet-text-content">Met persoonlijke <strong>cadeauwikkel</strong> en <strong>kaartje</strong></span>
+              </div>
+            </div>
+            <div class="id85-product-bullet">
+              <div class="id85-product-bullet-icon"></div>
+              <div class="id85-product-bullet-text">
+                <span class="id85-bullet-text-content">Elke bonbon een <strong>unieke smaakbeleving</strong></span>
+              </div>
+            </div>
+          `;
+
+          document.querySelector(
+            `${config.selector} > p:first-of-type`
+          ).innerHTML = updatedBulletPoints;
+
+          document.querySelectorAll(".cb-product-usps").forEach((element) => {
+            element.innerHTML = updatedUspTexts;
+          });
         },
 
         handleTracking: function () {
@@ -100,3 +183,5 @@
     }
   );
 })();
+
+// v1: https://www.chocoladebezorgd.nl/bonbons-bestellen/box-of-happiness-24-stuks/?_conv_eforce=1004168695.1004398359&utm_campaign=qa85
